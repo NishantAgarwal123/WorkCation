a646af66ada12cd0e08c65b87a6887dcddffc2a7
Nishant Agarwal <nishantag09@gmail.com>
2024-06-30 13:34:37 +0530

    added cards-grid section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import popularDestinations from "./data/popularDestination";
+import DestinationCard from "./components/DestinationCard";
 function App() {
   return (
     <div className="App">
-      <div className="grid lg:grid-cols-2 lg:bg-white 2xl:grid-cols-5">
+      <div className=" grid lg:grid-cols-2 bg-white 2xl:grid-cols-5 pb-16 lg:pb-0">
         <div className="pt-8 max-w-md mx-auto sm:max-w-xl sm:pt-12 lg:px-12 lg:py-24 2xl:col-span-2">
           <h1 className="text-2xl font-bold sm:text-3xl">
             Work<span className="text-indigo-500">cation</span>
@@ -32,6 +33,16 @@ function App() {
             src="https://plus.unsplash.com/premium_photo-1676637000058-96549206fe71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="imgage"
           />
+        </div>
+      </div>
+      
+      <div className="max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
+        <h2 className="text-xl text-gray-900">Popular destinations</h2>
+        <p className="mt-2 text-gray-600">A selection of great work-friendly cities with lots to see and explore.</p>
+        <div className="mt-6 grid gap-6 lg:grid-cols-2 xl:grid-cols-3">
+          {popularDestinations.map((destination) => (
+            <DestinationCard destination={destination} key={destination.city} />
+          ))}
         </div>
       </div>
     </div>
